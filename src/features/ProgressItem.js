9aec41c3ca4c841012c4e5d0c279b00ddcb52544
nikita-skobov <nskobov.websites@gmail.com>
2020-12-06 22:18:48 -0600

    adds error coloring to progress item if errored

--- src/features/ProgressItem.js
+++ src/features/ProgressItem.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 
-export function get_progress_percent(stages) {
+export function get_progress_state(stages) {
     const percent_per_stage = 100.0 / stages.length
     let display_percent = 0
+    let is_errored = false
 
     for (let i = 0; i < stages.length; i += 1) {
         const stage = stages[i]
         display_percent += (stage.percent / 100.0) * percent_per_stage
+
+        if (stage.errored != null) {
+            is_errored = true
+        }
 
         // if we reach a stage that is currently in progress
         // then every stage after it will have progress percent 0
@@ -15,14 +20,26 @@ export function get_progress_percent(stages) {
             break
         }
     }
-    return display_percent
+    return [display_percent, is_errored]
 }
 
 export function ProgressItem(props) {
     const { data } = props
-    const { stages, key, progress } = data
+    const {
+        stages,
+        key,
+        progress,
+        is_errored,
+    } = data
     console.log('I am progress item, here is data:')
     console.log(data)
+
+    let color = 'white'
+    let progress_color = 'bg-info'
+    if (is_errored) {
+        progress_color = 'bg-danger'
+        color = '#ff7272'
+    }
 
     return (
         <div
@@ -41,14 +58,14 @@ export function ProgressItem(props) {
                     width: '100%',
                     top: '50%',
                     transform: 'translateY(-50%)',
-                    color: 'white',
+                    color: color,
                     fontSize: '14px',
                 }}
             >
                 {key}
             </span>
             <div
-                class="progress-bar"
+                class={`progress-bar ${progress_color}`}
                 style={{width: `${progress}%`}}
                 role="progressbar"
                 aria-valuenow={progress}
