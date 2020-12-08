b481a5de59ecc702660a2b02881be0080e2c04d3
nikita-skobov <nskobov.websites@gmail.com>
2020-12-07 22:22:38 -0600

    adds start and duration fields to add item form

--- src/features/AddItemForm.js
+++ src/features/AddItemForm.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 async function add_progress_item(json) {
     try {
+        console.log(json)
         const result = await axios.post('http://localhost:4000/download', json)
         console.log(result)
         return true
@@ -25,15 +26,28 @@ export class AddItemForm extends React.Component {
             const { elements } = target
             const [
                 url_input,
+                start_input,
+                duration_input,
                 name_input,
             ] = elements
 
             const url = url_input.value
             const name = name_input.value
+            const start = start_input.value
+            const duration = duration_input.value
+
+            const start_num = Number.parseInt(start)
+            const duration_num = Number.parseInt(duration)
 
             const submit_json = { url }
             if (name) {
                 submit_json.name = name
+            }
+            if (!Number.isNaN(start_num)) {
+                submit_json.start = start_num
+            }
+            if (!Number.isNaN(duration_num)) {
+                submit_json.duration = duration_num
             }
             add_progress_item(submit_json)
                 .then(resp => {
@@ -52,7 +66,16 @@ export class AddItemForm extends React.Component {
                 <div class="form-group">
                     <label for="dl_url">URL</label>
                     <input type="text" class="form-control" id="dl_url" />
-                    {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <div class="form-group">
+                    <label for="dl_start">Start position(in seconds)</label>
+                    <input type="number" class="form-control" id="dl_start" />
+                    <small id="dl_start_help" class="form-text text-muted">Optional. leave blank if you want your clip to start at the beginning.</small>
+                </div>
+                <div class="form-group">
+                    <label for="dl_duration">Duration (in seconds)</label>
+                    <input type="number" class="form-control" id="dl_duration" />
+                    <small id="dl_duration_help" class="form-text text-muted">Optional. leave blank if you want to only clip a certain duration. Can be combined with "Start" to create clips of any time range.</small>
                 </div>
                 <div class="form-group">
                     <label for="dl_name">Name</label>
